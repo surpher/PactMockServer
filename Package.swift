00d0b55d002e7a91c819c29db825a83b80af49c6
Marko Justinek <marko.justinek@ittybittyapps.com>
2021-04-30 11:09:58 +1000

    chore: Refactor Package manifest

--- Package.swift
+++ Package.swift
@@ -5,29 +5,33 @@ import PackageDescription
 
 let package = Package(
 	name: "PactMockServer",
+
+	// MARK: - Platforms
+
 	platforms: [
 		.macOS(.v10_12),
 		.iOS(.v12),
 		.tvOS(.v12)
 	],
+
+	// MARK: - Products
+
 	products: [
-		// Products define the executables and libraries produced by a package, and make them visible to other packages.
-		.library(
-			name: "PactMockServer",
-			targets: ["PactMockServer"]
-		),
+		.library(name: "PactMockServer", targets: ["PactMockServer"]),
 	],
+
+	// MARK: - Dependencies
+
 	dependencies: [
-		// Dependencies declare other packages that this package depends on.
-		// .package(url: /* package url */, from: "1.0.0"),
 	],
+
+	// MARK: - Targets
+
 	targets: [
-		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
-		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
-		.target(
-			name: "PactMockServer",
-			path: "./Sources"
-		)
+		.target(name: "PactMockServer", path: "./Sources")
 	],
+
+	// MARK: - Other versions
+	
 	swiftLanguageVersions: [.v5]
 )
